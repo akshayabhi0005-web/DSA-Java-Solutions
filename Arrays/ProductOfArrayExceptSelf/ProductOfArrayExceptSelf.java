5301331c57a78567659f19d1a2421a4ca35d3f08
Akshay N <akshayabhi0005@gmail.com>
2026-09-15 20:41:20 +0530

    Implement productExceptSelf method in Solution class

---
+++ Arrays/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Product of elements to the left
+        int leftProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct = leftProduct * nums[i];
+        }
+
+        // Product of elements to the right
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+}
